c74531de6e829a15968e8cb1ae6a387535e8b616
SLB SMKMIL <129854483+slbsmkmil@users.noreply.github.com>
2024-08-31 19:06:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('', 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
